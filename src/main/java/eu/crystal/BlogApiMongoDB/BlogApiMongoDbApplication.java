24b28e10c8ba4bd602b825d3ba698a8e51d194c3
Robert Valentin <robert.carp@crystal-system.eu>
2021-10-15 12:30:59 +0300

    Update BlogApiMongoDbApplication.java

--- src/main/java/eu/crystal/BlogApiMongoDB/BlogApiMongoDbApplication.java
+++ src/main/java/eu/crystal/BlogApiMongoDB/BlogApiMongoDbApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApiMongoDBApplication {
+public class BlogApiMongoDbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogApiMongoDBApplication.class, args);
+		SpringApplication.run(BlogApiMongoDbApplication.class, args);
 	}
 
 }
