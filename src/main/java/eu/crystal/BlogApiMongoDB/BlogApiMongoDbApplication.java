552d84878db188330cc975c69075ca0bdd4616e6
Robert Valentin <robert.carp@crystal-system.eu>
2021-10-15 12:14:10 +0300

    added to pom

--- src/main/java/eu/crystal/BlogApiMongoDB/BlogApiMongoDbApplication.java
+++ src/main/java/eu/crystal/BlogApiMongoDB/BlogApiMongoDbApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApiMongoDbApplication {
+public class BlogApiMongoDBApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogApiMongoDbApplication.class, args);
+		SpringApplication.run(BlogApiMongoDBApplication.class, args);
 	}
 
 }
